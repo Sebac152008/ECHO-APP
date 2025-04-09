de15e2cc6a8eb86962c9c154091a6dc6f436adc7
unknown <sebac152008@gmail.com>
2025-04-09 11:53:03 -0400

    Actualice la APP a WPF, Agregue iconos nuevos, y un mejor diseño

---
+++ WPF-ECHO/SplashScreen.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WPF_ECHO
+{
+    /// <summary>
+    /// Lógica de interacción para SplashScreen.xaml
+    /// </summary>
+    public partial class SplashScreen : Window
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+
+
+            // Inicia el splash screen al cargar la ventana
+            DispatcherTimer timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(3) // Duración del splash screen
+            };
+            timer.Tick += (s, e) =>
+            {
+                timer.Stop();
+                ShowMainContent(); // Mostrar el contenido principal
+            };
+            timer.Start();
+        }
+
+
+        private void ShowMainContent()
+        {
+            SplashGrid.Visibility = Visibility.Collapsed;  // Ocultar Splash Screen
+            MainContent.Content = new View.MenuNav();       // Cargar MenuNav en MainContent
+            MainContent.Visibility = Visibility.Visible;
+        }
+
+    }
+}
---
+++ WPF-ECHO/ViewModels/MainViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WPF_ECHO.View;
+
+namespace WPF_ECHO.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private ViewModelBase _currentChildView;
+
+        public ViewModelBase CurrentChildView
+        {
+            get { return _currentChildView; }
+            set { _currentChildView = value; 
+            OnPropertyChanged(nameof(CurrentChildView)); }
+        }
+
+        //Commands
+
+        public ICommand ShowInicioViewCommand { get; }
+        public ICommand ShowAcercaDeCommand { get; }
+        public ICommand ShowMenuNavCommand { get; }
+
+        public MainViewModel()
+        {
+            //Initialize commands
+            ShowInicioViewCommand = new ViewModelCommand(ExecuteShowInicioViewCommand);
+            ShowAcercaDeCommand = new ViewModelCommand(ExecuteShowAcercaDeCommand);
+            ShowMenuNavCommand = new ViewModelCommand(ExecuteShowMenuNavCommand);
+
+            //Defaul view
+
+            ExecuteShowInicioViewCommand(null);
+        }
+
+        private void ExecuteShowMenuNavCommand(object obj)
+        {
+            CurrentChildView = new MenuNav();
+        }
+
+        private void ExecuteShowAcercaDeCommand(object obj)
+        {
+            CurrentChildView = new AcercaDeView();
+        }
+
+        private void ExecuteShowInicioViewCommand(object obj)
+        {
+            CurrentChildView = new InicioView();
+        }
+    }
+}
